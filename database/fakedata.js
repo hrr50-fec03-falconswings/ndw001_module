a98e1586233f61f6e0d92f5acd14a2655e58d71b
Nathan Wong <natdanwongielhan@yahoo.com>
2021-01-15 11:49:47 -0800

    Added comments into code

--- database/fakedata.js
+++ database/fakedata.js
@@ -8,8 +8,12 @@ Products
 Pictures
 (picID, productID, imageURL)
 */
+
 // let xbox = '(Xbox Series S, 4.5, 654, 1473, 299.00, 2)';
 // let xboxPictures ;
+
+
+// Creates fake data
 for (let i = 1; i <= 100; i++) {
   let name = faker.commerce.productName();
   let price = faker.commerce.price();
@@ -18,6 +22,7 @@ for (let i = 1; i <= 100; i++) {
   let randComments = Math.floor(Math.random() * Math.floor(200));
   let randRemain = Math.floor(Math.random() * Math.floor(10));
   let picArr = [];
+  // creates 7 pictures to add into images
   for(let k = 0; k < 7; k++){
     let picture = faker.image.image();
     picArr.push(picture);
@@ -25,7 +30,7 @@ for (let i = 1; i <= 100; i++) {
   let pictureArr = JSON.stringify(picArr);
   let queryString = 'insert into Products (name, rating, ratings_amt, comments_amt, price, amt_left, images) values (?, ?, ?, ?, ?, ?, ?)'
   let oneProduct = [name, rating, randRatings, randComments, price, randRemain, pictureArr];
-  // debugger;
+  // each loop will CREATE to db using the randomly generated data above
   db.query(queryString, oneProduct, (err, results) => {
     if(err){
       console.log(err);
@@ -34,4 +39,3 @@ for (let i = 1; i <= 100; i++) {
     }
   });
 }
-// do the questions marks
